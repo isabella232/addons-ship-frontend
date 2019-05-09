587887692b11438e28cfcd8df118af20d81f49b0
Simon Márton <simon.marton@bitrise.io>
2019-05-09 16:52:29 +0200

    Added Beam

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,29 @@ import withRedux from 'next-redux-wrapper';
 
 import makeStore from '../store';
 
+import '@/assets/style/index.scss';
+
 interface ShipAppProps extends DefaultAppIProps {
   store: Store;
+  appSlug: string;
+  appName: string;
 }
 
 class ShipApp extends App<ShipAppProps> {
   static async getInitialProps({ Component, ctx }: NextAppContext) {
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
-    return { pageProps };
+    return { pageProps, appSlug: 'my-super-app', appName: 'My Super App' };
+  }
+
+  componentDidMount() {
+    const { Beam } = require('@bitrise/beam');
+    const { appSlug, appName } = this.props;
+
+    Beam.init({
+      app_name: appName,
+      app_slug: appSlug
+    });
   }
 
   render() {
