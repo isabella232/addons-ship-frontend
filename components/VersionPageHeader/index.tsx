af69646d945d0e2f1664a4cd86b1ec64f2fc8961
József Erős <jozsef.eros@bitrise.io>
2019-05-17 15:51:58 +0200

    Add version page header component

---
+++ components/VersionPageHeader/index.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+import css from './style.scss';
+import SVG from 'react-inlinesvg';
+
+interface VersionPageHeaderProps {
+  appName: string;
+  versionIconUrl: string;
+  children: ReactNode;
+}
+
+export default ({ appName, versionIconUrl, children }: VersionPageHeaderProps) => (
+  <div className={css.versionPageHeader}>
+    <a href="/" className={css.appName}>
+      {appName}
+    </a>
+    <div className={css.versionNameWrapper}>
+      <SVG src={versionIconUrl} className={css.versionIcon} />
+      <h2 className={css.versionName}>{children}</h2>
+    </div>
+  </div>
+);
