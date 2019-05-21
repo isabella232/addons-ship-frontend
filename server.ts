82fbaee5c1dea49cf2aa8acf0fc7cca9ff7f9446
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-21 14:52:57 +0200

    Add missing semi-colons

--- server.ts
+++ server.ts
@@ -1,36 +1,36 @@
 import * as next from 'next';
 import * as express from 'express';
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
   server.get('/', (req, res) =>
     app.render(req, res, '/', req.params)
-  )
+  );
 
   server.get('/apps/:appSlug', (req, res) =>
     app.render(req, res, '/app', req.params)
-  )
+  );
 
   server.get('/apps/:appSlug/versions/:versionId', (req, res) =>
     app.render(req, res, '/version', req.params)
-  )
+  );
 
   server.get('/apps/:appSlug/versions/:versionId/public', (req, res) =>
     app.render(req, res, '/version', req.params)
-  )
+  );
 
   server.get('*', (req, res) => {
     return handle(req, res)
-  })
+  });
 
   server.listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+  });
+});
