94f6d88b15cb412a7f3d5473a7450157ee211fe6
Simon Márton <simon.marton@bitrise.io>
2019-05-24 16:04:06 +0200

    Added camelizeKeys

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,7 @@
+import mapKeys from 'lodash/mapKeys';
+import camelCase from 'lodash/camelCase';
+
 export const actionTypeCreator = (prefix: string) => (actionType: TemplateStringsArray) => `${prefix}_${actionType}`;
+
+export const camelizeKeys = <T>(input: Object): T =>
+  (mapKeys(input, (_, key: string) => camelCase(key)) as unknown) as T;
--- utils/utils.spec.ts
+++ utils/utils.spec.ts
@@ -1,4 +1,4 @@
-import { actionTypeCreator } from './';
+import { actionTypeCreator, camelizeKeys } from './';
 
 describe('actionTypeCreator', () => {
   it('returns a function', () => {
@@ -12,5 +12,23 @@ describe('actionTypeCreator', () => {
 
     expect(a`SOME_ACTION`).toBe('PREFIX_SOME_ACTION');
     expect(a`OTHER_ACTION`).toBe('PREFIX_OTHER_ACTION');
+  });
+});
+
+describe('camelizeKeys', () => {
+  it('shallow converts keys to camelCase', () => {
+    const input = {
+      CONSTANT_CASE: 'aaa',
+      snake_case: 123,
+      PascalCase: 'bbb'
+    };
+
+    const expected = {
+      constantCase: 'aaa',
+      snakeCase: 123,
+      pascalCase: 'bbb'
+    };
+
+    expect(camelizeKeys(input)).toEqual(expected);
   });
 });
