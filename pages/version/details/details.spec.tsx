4b2bc7887fb3e94e3dfaaaf00305101ff55e417f
Simon Márton <simon.marton@bitrise.io>
2019-05-31 15:02:13 +0200

    Resposive version page

--- pages/version/details/details.spec.tsx
+++ pages/version/details/details.spec.tsx
@@ -1,3 +1,5 @@
+jest.mock('@/utils/media');
+
 import { shallow, mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 
@@ -5,13 +7,24 @@ import DetailsView from './view';
 import { AppVersionDetails } from './';
 import { mockAppVersion } from '@/mocks';
 
+import { mediaQuery } from '@/utils/media';
+
 describe('AppDetailsView', () => {
-  it('renders the details view correctly', () => {
+  it('renders the details view correctly for desktop', () => {
+    (mediaQuery as jest.Mock).mockReturnValue([true]);
+
+    const tree = toJSON(mount(<DetailsView appVersion={mockAppVersion} showTooltips />));
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the details view correctly for mobile', () => {
+    (mediaQuery as jest.Mock).mockReturnValue([false]);
     const tree = toJSON(mount(<DetailsView appVersion={mockAppVersion} showTooltips />));
     expect(tree).toMatchSnapshot();
   });
 
   it('AppVersionDetails', () => {
+    (mediaQuery as jest.Mock).mockReturnValue([true]);
     const tree = toJSON(shallow(<AppVersionDetails appVersion={mockAppVersion} />));
     expect(tree).toMatchSnapshot();
   });
