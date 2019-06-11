a905c696000a2b0f7224b79c6e2c920567869408
József Erős <jozsef.eros@bitrise.io>
2019-06-11 13:00:48 +0200

    Move iOS specific form to separate component

---
+++ pages/version/details/view/form-ios.tsx
@@ -0,0 +1,198 @@
+import { Fragment } from 'react';
+import formatDate from 'date-fns/format';
+import prettyBytes from 'pretty-bytes';
+import {
+  Base,
+  Flex,
+  Text,
+  Icon,
+  InputLabel,
+  Textarea,
+  Divider,
+  Tooltip,
+  InputContainer,
+  Input
+} from '@bitrise/bitkit';
+
+import { AppVersion } from '@/models';
+import Dropzone from '@/components/Dropzone';
+import SmallTabs from '@/components/SmallTabs';
+
+type Props = {
+  appVersion: AppVersion;
+  availableDevices: Array<{
+    key: string;
+    value: string;
+  }>;
+  selectedDeviceIdForScreenshots: string;
+  screenshots?: File[];
+  showTooltips: boolean;
+  onScreenshotAdded: (deviceId: string, screenshots: File[]) => void;
+  removeScreenshot: (deviceId: string, screenshot: File) => void;
+  onDeviceSelected: (key: string) => void;
+};
+
+export default ({
+  appVersion,
+  availableDevices,
+  selectedDeviceIdForScreenshots,
+  screenshots,
+  showTooltips,
+  onScreenshotAdded,
+  removeScreenshot,
+  onDeviceSelected
+}: Props) => (
+  <Fragment>
+    <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
+      <InputLabel>Description</InputLabel>
+      <Text size="x2" weight="medium" color="gray-7" align="end">
+        1235
+      </Text>
+    </Flex>
+    <Textarea name="description" defaultValue={appVersion.description} />
+
+    <Base paddingVertical="x4">
+      <Flex direction="horizontal" alignChildrenVertical="middle">
+        <Text color="grape-4" size="x3" weight="bold">
+          Screenshots
+        </Text>
+        <Icon name="Support" color="grape-3" paddingHorizontal="x1" size="2rem" />
+      </Flex>
+
+      <SmallTabs items={availableDevices} selected={selectedDeviceIdForScreenshots} onSelect={onDeviceSelected} />
+      <Dropzone
+        files={screenshots}
+        onFilesAdded={files => onScreenshotAdded(selectedDeviceIdForScreenshots, files)}
+        removeFile={file => removeScreenshot(selectedDeviceIdForScreenshots, file)}
+      />
+    </Base>
+
+    <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
+      <InputLabel>What's new</InputLabel>
+      <Text size="x2" weight="medium" color="gray-7" align="end">
+        1235
+      </Text>
+    </Flex>
+    <Textarea name="whatsNew" defaultValue={appVersion.whatsNew} />
+
+    <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+    <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
+      <InputLabel>Promotional Text</InputLabel>
+      <Text size="x2" weight="medium" color="gray-7" align="end">
+        1235
+      </Text>
+    </Flex>
+    <Textarea name="promotionalText" defaultValue={appVersion.promotionalText} />
+
+    <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+    <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
+      <InputLabel>Keywords</InputLabel>
+      <Text size="x2" weight="medium" color="gray-7" align="end">
+        1235
+      </Text>
+    </Flex>
+    <Textarea name="keywords" defaultValue={appVersion.keywords} />
+
+    <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+    <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
+      <InputLabel>Review Notes</InputLabel>
+      <Text size="x2" weight="medium" color="gray-7" align="end">
+        1235
+      </Text>
+    </Flex>
+    <Textarea name="reviewNotes" defaultValue={appVersion.reviewNotes} />
+
+    <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+    <Flex direction="horizontal" alignChildrenVertical="middle">
+      <InputLabel>Distribution Type: {appVersion.distributionType}</InputLabel>
+      {showTooltips && (
+        <Tooltip title="This is the tooltip">
+          {({ ref, ...rest }) => (
+            <Icon {...rest} paddingHorizontal="x1" innerRef={ref} color="grape-3" name="Coffee" size="1.5rem" />
+          )}
+        </Tooltip>
+      )}
+    </Flex>
+    {appVersion.distributionType === 'development' && (
+      <Text size="x3" weight="medium" color="gray-7">
+        The app was signed with a Development Provisioning Profile which means that it{' '}
+        <Text weight="bold">can only be installed on devices which are included in the Provisioning Profile</Text>
+      </Text>
+    )}
+
+    <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+    <Flex direction="horizontal" grow gap="x6">
+      <Flex grow>
+        <InputLabel>Support URL</InputLabel>
+        <InputContainer>
+          <Input name="supportUrl" defaultValue={appVersion.supportUrl} />
+        </InputContainer>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>App Title</InputLabel>
+        <Text size="x3">{appVersion.appName}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Version</InputLabel>
+        <Text size="x3">v{appVersion.version}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Certificate Expires</InputLabel>
+        <Text size="x3">{formatDate(appVersion.certificateExpires, 'MMM DD')}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Minimum OS</InputLabel>
+        <Text size="x3">{appVersion.minimumOs}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Scheme</InputLabel>
+        <Text size="x3">{appVersion.scheme}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+      </Flex>
+      <Flex grow>
+        <InputLabel>Marketing URL</InputLabel>
+        <InputContainer>
+          <Input name="marketingUrl" defaultValue={appVersion.marketingUrl} />
+        </InputContainer>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Bundle ID</InputLabel>
+        <Text size="x3">{appVersion.bundleId}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Build Number</InputLabel>
+        <Text size="x3">{appVersion.buildNumber}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Size</InputLabel>
+        <Text size="x3">{prettyBytes(appVersion.size)}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Supported Device Types</InputLabel>
+        <Text size="x3">{appVersion.supportedDeviceTypes.join(', ')}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+
+        <InputLabel>Configuration</InputLabel>
+        <Text size="x3">{appVersion.configuration}</Text>
+
+        <Divider color="gray-2" direction="horizontal" margin="x4" />
+      </Flex>
+    </Flex>
+  </Fragment>
+);
--- pages/version/details/view/index.tsx
+++ pages/version/details/view/index.tsx
@@ -1,27 +1,11 @@
 import formatDate from 'date-fns/format';
-import prettyBytes from 'pretty-bytes';
-import {
-  Base,
-  Divider,
-  Textarea,
-  Flex,
-  Text,
-  Icon,
-  Image,
-  Notification,
-  Tooltip,
-  Input,
-  InputContainer,
-  InputLabel
-} from '@bitrise/bitkit';
-import { TypeIconName } from '@bitrise/bitkit/Icon/tsx';
+import { Base, Flex, Text, Icon, Image, Notification } from '@bitrise/bitkit';
 
 import { AppVersion } from '@/models';
 import { mediaQuery } from '@/utils/media';
-import Dropzone from '@/components/Dropzone';
-import SmallTabs from '@/components/SmallTabs';
 
 import Sidebar from './sidebar';
+import FormIos from './form-ios';
 
 type Props = {
   appVersion: AppVersion;
@@ -51,8 +35,6 @@ export default ({
   onChange,
   onDeviceSelected
 }: Props) => {
-  const iconName: TypeIconName = appVersion.platform === 'ios' ? 'PlatformsApple' : 'PlatformsAndroid';
-
   const [isDesktop] = mediaQuery('60rem');
 
   const onFormChange = ({ target: { name, value } }: any) => {
@@ -76,7 +58,7 @@ export default ({
 
               <Flex direction="vertical" alignChildrenVertical="middle" paddingHorizontal="x6">
                 <Flex direction="horizontal" alignChildren="middle">
-                  <Icon color="grape-4" name={iconName} size="1.5rem" />
+                  <Icon color="grape-4" name="PlatformsApple" size="1.5rem" />
                   <Text letterSpacing="x2" size="x6" weight="bold" color="grape-4" paddingHorizontal="x2">
                     {appVersion.appName}
                   </Text>
@@ -90,159 +72,18 @@ export default ({
               </Flex>
             </Flex>
 
-            <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
-              <InputLabel>Description</InputLabel>
-              <Text size="x2" weight="medium" color="gray-7" align="end">
-                1235
-              </Text>
-            </Flex>
-            <Textarea name="description" defaultValue={appVersion.description} />
-
-            <Base paddingVertical="x4">
-              <Flex direction="horizontal" alignChildrenVertical="middle">
-                <Text color="grape-4" size="x3" weight="bold">
-                  Screenshots
-                </Text>
-                <Icon name="Support" color="grape-3" paddingHorizontal="x1" size="2rem" />
-              </Flex>
-
-              <SmallTabs items={availableDevices} selected={deviceId} onSelect={onDeviceSelected} />
-              <Dropzone
-                files={screenshots}
-                onFilesAdded={files => onScreenshotAdded(deviceId, files)}
-                removeFile={file => removeScreenshot(deviceId, file)}
+            {appVersion.platform === 'ios' && (
+              <FormIos
+                appVersion={appVersion}
+                availableDevices={availableDevices}
+                selectedDeviceIdForScreenshots={deviceId}
+                screenshots={screenshots}
+                showTooltips={showTooltips}
+                onScreenshotAdded={onScreenshotAdded}
+                removeScreenshot={removeScreenshot}
+                onDeviceSelected={onDeviceSelected}
               />
-            </Base>
-
-            <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
-              <InputLabel>What's new</InputLabel>
-              <Text size="x2" weight="medium" color="gray-7" align="end">
-                1235
-              </Text>
-            </Flex>
-            <Textarea name="whatsNew" defaultValue={appVersion.whatsNew} />
-
-            <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-            <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
-              <InputLabel>Promotional Text</InputLabel>
-              <Text size="x2" weight="medium" color="gray-7" align="end">
-                1235
-              </Text>
-            </Flex>
-            <Textarea name="promotionalText" defaultValue={appVersion.promotionalText} />
-
-            <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-            <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
-              <InputLabel>Keywords</InputLabel>
-              <Text size="x2" weight="medium" color="gray-7" align="end">
-                1235
-              </Text>
-            </Flex>
-            <Textarea name="keywords" defaultValue={appVersion.keywords} />
-
-            <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-            <Flex direction="horizontal" alignChildrenHorizontal="between" alignChildrenVertical="middle">
-              <InputLabel>Review Notes</InputLabel>
-              <Text size="x2" weight="medium" color="gray-7" align="end">
-                1235
-              </Text>
-            </Flex>
-            <Textarea name="reviewNotes" defaultValue={appVersion.reviewNotes} />
-
-            <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-            <Flex direction="horizontal" alignChildrenVertical="middle">
-              <InputLabel>Distribution Type: {appVersion.distributionType}</InputLabel>
-              {showTooltips && (
-                <Tooltip title="This is the tooltip">
-                  {({ ref, ...rest }) => (
-                    <Icon {...rest} paddingHorizontal="x1" innerRef={ref} color="grape-3" name="Coffee" size="1.5rem" />
-                  )}
-                </Tooltip>
-              )}
-            </Flex>
-            {appVersion.distributionType === 'development' && (
-              <Text size="x3" weight="medium" color="gray-7">
-                The app was signed with a Development Provisioning Profile which means that it{' '}
-                <Text weight="bold">
-                  can only be installed on devices which are included in the Provisioning Profile
-                </Text>
-              </Text>
             )}
-
-            <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-            <Flex direction="horizontal" grow gap="x6">
-              <Flex grow>
-                <InputLabel>Support URL</InputLabel>
-                <InputContainer>
-                  <Input name="supportUrl" defaultValue={appVersion.supportUrl} />
-                </InputContainer>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>App Title</InputLabel>
-                <Text size="x3">{appVersion.appName}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Version</InputLabel>
-                <Text size="x3">v{appVersion.version}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Certificate Expires</InputLabel>
-                <Text size="x3">{formatDate(appVersion.certificateExpires, 'MMM DD')}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Minimum OS</InputLabel>
-                <Text size="x3">{appVersion.minimumOs}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Scheme</InputLabel>
-                <Text size="x3">{appVersion.scheme}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-              </Flex>
-              <Flex grow>
-                <InputLabel>Marketing URL</InputLabel>
-                <InputContainer>
-                  <Input name="marketingUrl" defaultValue={appVersion.marketingUrl} />
-                </InputContainer>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Bundle ID</InputLabel>
-                <Text size="x3">{appVersion.bundleId}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Build Number</InputLabel>
-                <Text size="x3">{appVersion.buildNumber}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Size</InputLabel>
-                <Text size="x3">{prettyBytes(appVersion.size)}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Supported Device Types</InputLabel>
-                <Text size="x3">{appVersion.supportedDeviceTypes.join(', ')}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-
-                <InputLabel>Configuration</InputLabel>
-                <Text size="x3">{appVersion.configuration}</Text>
-
-                <Divider color="gray-2" direction="horizontal" margin="x4" />
-              </Flex>
-            </Flex>
           </form>
         </Flex>
 
