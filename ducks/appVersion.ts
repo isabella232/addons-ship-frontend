37d5dd7d1f14142150db2519846a3d1997e3933c
Simon Márton <simon.marton@bitrise.io>
2019-06-12 18:33:00 +0200

    Upload screenshots actions

--- ducks/appVersion.ts
+++ ducks/appVersion.ts
@@ -3,7 +3,9 @@ import { createAction, createReducer } from 'deox';
 
 import api from '@/services/ship-api';
 import { actionTypeCreator } from '@/utils';
+import { uploadFileToS3 } from '@/utils/file';
 import { AppVersion } from '@/models';
+import { Uploadable } from '@/models/uploadable';
 
 const $ = actionTypeCreator('APP_VERSION');
 
@@ -41,6 +43,39 @@ export const updateAppVersion = Object.assign(_updateAppVersion, {
   next: createAction($`UPDATE_NEXT`),
   complete: createAction($`UPDATE_COMPLETE`, resolve => (appVersion: AppVersion) => resolve(appVersion)),
   error: createAction($`UPDATE_ERROR`, resolve => (error: Error) => resolve(error))
+});
+
+// Upload screenshots
+const _uploadScreenshots = (appSlug: string, versionId: string, screenshots: Uploadable[], files: File[]) => async (
+  dispatch: Dispatch
+) => {
+  dispatch(uploadScreenshots.next());
+
+  try {
+    // Get presigned Urls
+    const withPresignedUrl = await api.uploadScreenshots(appSlug, versionId, screenshots);
+
+    // Upload images to S3
+    await Promise.all(
+      withPresignedUrl.map(({ filename, uploadUrl }) => {
+        const file = files.find(({ name }) => name === filename);
+
+        return uploadFileToS3(file, uploadUrl);
+      })
+    );
+
+    // Mark screenshots as uploaded
+    await api.uploadedScreenshots(appSlug, versionId);
+    dispatch(uploadScreenshots.complete());
+  } catch (error) {
+    dispatch(uploadScreenshots.error(error));
+  }
+};
+
+export const uploadScreenshots = Object.assign(_uploadScreenshots, {
+  next: createAction($`UPLOAD_SCREENSHOTS_NEXT`),
+  complete: createAction($`UPLOAD_SCREENSHOTS_COMPLETE`),
+  error: createAction($`UPLOAD_SCREENSHOTS_ERROR`, resolve => (error: Error) => resolve(error))
 });
 
 const defaultState = null as AppVersionState;
