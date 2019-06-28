c2f7d4a55ae94eb556ac9b0b4f69387f3678cb36
József Erős <jozsef.eros@bitrise.io>
2019-06-28 12:44:55 +0200

    Remove white background with padding from dropzone

--- components/Dropzone/index.tsx
+++ components/Dropzone/index.tsx
@@ -48,43 +48,41 @@ export default ({
 
   return (
     <Card>
-      <Base className={css.dropzoneCard} padding="x3">
+      <Base backgroundColor="gray-1" className={css.dropzoneCard} padding="x3">
         <input {...getInputProps()} />
-        <Base backgroundColor="gray-1" padding="x3">
-          <Flex direction="horizontal" height={384} className={css.scrollableWrapper}>
-            <Flex direction="horizontal" height={400} className={css.scrollable}>
-              <Flex direction="horizontal" height={384} className={css.scrollableInner}>
-                {(!hasFiles || isMultiple) && (
-                  <div
-                    className={cx(css.dropzone, {
-                      [css.dropzoneActive]: isDragActive,
-                      [css.dropzoneHasFiles]: hasFiles
-                    })}
-                    {...getRootProps()}
-                  >
-                    {hasFiles ? (
-                      <Icon name="PlusAdd" color="grape-3" />
-                    ) : (
-                      <Flex direction="vertical" alignChildrenHorizontal="middle">
-                        <Text size="x5" color="gray-7" align="middle" weight="medium">
-                          {instructionsBeginning} <br />
-                          or
+        <Flex direction="horizontal" height={384} className={css.scrollableWrapper}>
+          <Flex direction="horizontal" height={400} className={css.scrollable}>
+            <Flex direction="horizontal" height={384} className={css.scrollableInner}>
+              {(!hasFiles || isMultiple) && (
+                <div
+                  className={cx(css.dropzone, {
+                    [css.dropzoneActive]: isDragActive,
+                    [css.dropzoneHasFiles]: hasFiles
+                  })}
+                  {...getRootProps()}
+                >
+                  {hasFiles ? (
+                    <Icon name="PlusAdd" color="grape-3" />
+                  ) : (
+                    <Flex direction="vertical" alignChildrenHorizontal="middle">
+                      <Text size="x5" color="gray-7" align="middle" weight="medium">
+                        {instructionsBeginning} <br />
+                        or
+                      </Text>
+                      <Flex direction="horizontal" alignChildrenVertical="middle">
+                        <Icon name="PlusAdd" color="grape-3" paddingHorizontal="x1" />
+                        <Text size="x3" color="grape-3" align="middle" weight="medium">
+                          {instructionsAction}
                         </Text>
-                        <Flex direction="horizontal" alignChildrenVertical="middle">
-                          <Icon name="PlusAdd" color="grape-3" paddingHorizontal="x1" />
-                          <Text size="x3" color="grape-3" align="middle" weight="medium">
-                            {instructionsAction}
-                          </Text>
-                        </Flex>
                       </Flex>
-                    )}
-                  </div>
-                )}
-                {thumbs}
-              </Flex>
+                    </Flex>
+                  )}
+                </div>
+              )}
+              {thumbs}
             </Flex>
           </Flex>
-        </Base>
+        </Flex>
       </Base>
     </Card>
   );
