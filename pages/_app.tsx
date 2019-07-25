fcbb7f27dd741ab024838afd0bcbf1342959f48c
József Erős <jozsef.eros@bitrise.io>
2019-07-25 10:18:38 +0200

    Add settings notification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { Store } from 'redux';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import nookies from 'nookies';
-import { ProgressBitbot, Base } from '@bitrise/bitkit';
+import { ProgressBitbot, Base, Notification, Text } from '@bitrise/bitkit';
 
 import makeStore from '../store';
 import { setToken } from '@/ducks/auth';
@@ -75,6 +75,12 @@ class ShipApp extends App<ShipAppProps> {
         <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
+        <Notification type="inform" icon="Lightbulb">
+          <Text size="x3" weight="bold" margin="x1">Setup Publishing</Text>
+          <Text>
+            We really recommend you to setup publishing as a first step. You only need to do this once per application, then you will be able to publish all versions to App Store Connect or Google Play Console.
+          </Text>
+        </Notification>
       </Container>
     );
   }
