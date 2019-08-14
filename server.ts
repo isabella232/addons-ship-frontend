0cae9feea6c677481f2f7546a26b4a1db05f06de
Simon Márton <simon.marton@bitrise.io>
2019-08-14 13:37:54 +0200

    Added utility server call for api

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
 import * as next from 'next';
 import * as express from 'express';
+import fetch from 'node-fetch';
+import { pick } from 'lodash';
 
 import { AppVersionPageQuery } from './models';
 import { AppSettingsPageQuery } from './models/settings';
+
+const shipApiUrl = process.env.SHIP_API_URL || 'http://localhost:3003';
 
 const port = parseInt(<string>process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -30,6 +34,30 @@ app.prepare().then(() => {
   server.get(`${appPath}/settings/:selectedTab?`, (req, res) =>
     app.render(req, res, '/settings', req.params as AppSettingsPageQuery)
   );
+
+  server.all(`/api/resources`, async ({ query, headers, method }, res) => {
+    const { path } = query;
+
+    if (!path) {
+      throw new Error('Missing query param `path`');
+    }
+
+    const fullUrl = `${shipApiUrl}/resources/${path}`,
+      safeHeaders = pick(headers as { [key: string]: string }, [
+        'authorization',
+        'accept',
+        'accept-encoding',
+        'accept-language'
+      ]);
+
+    const resp = await fetch(fullUrl, {
+      method,
+      headers: safeHeaders
+    }).then(res => res.text());
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(resp);
+  });
 
   server.get('*', (req, res) => handle(req, res));
 
