dd9b5f016932122f7f800a41a881991c64fa14f5
Simon Márton <simon.marton@bitrise.io>
2019-08-29 16:57:30 +0200

    Added Squirecle component

---
+++ components/Squircle/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, BaseProps } from '@bitrise/bitkit';
+
+interface Props extends BaseProps {
+  fatness?: number;
+  objectFit?: string;
+}
+
+export default ({ objectFit = 'covver', fatness = 0.05, ...props }: Props) => {
+  const s = Math.min(Math.max(fatness, 0), 0.5);
+
+  const path = `M .5,0
+    C ${s},0 0,${s} 0,.5
+    0,${1 - s} ${s},1 .5,1
+    ${1 - s},1 1,${1 - s} 1,.5
+    1,${s} ${1 - s},0 .5,0
+    Z`;
+  const id = `squircle-${fatness}`;
+
+  return (
+    <div>
+      <svg width="0" height="0">
+        <defs>
+          <clipPath id={id} clipPathUnits="objectBoundingBox">
+            <path d={path} />
+          </clipPath>
+        </defs>
+      </svg>
+
+      <Image {...props} style={{ clipPath: `url(#${id})`, objectFit }} />
+    </div>
+  );
+};
