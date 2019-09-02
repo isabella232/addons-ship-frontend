7247aab58ded2a31f95b12da7d7c7b98d8148152
József Erős <jozsef.eros@bitrise.io>
2019-09-02 12:52:28 +0200

    Implement alternative tooltip for Android screenshots

--- pages/version/details/view/form-android.tsx
+++ pages/version/details/view/form-android.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import formatDate from 'date-fns/format';
 import prettyBytes from 'pretty-bytes';
-import { Base, Flex, Text, Icon, InputLabel, Textarea, Divider } from '@bitrise/bitkit';
+import { Base, Flex, Text, Icon, InputLabel, Textarea, Divider, Notification } from '@bitrise/bitkit';
 
 import { AppVersion, Screenshot } from '@/models';
 import Dropzone from '@/components/Dropzone';
@@ -72,6 +72,11 @@ export default ({
         <Text color="grape-4" size="x3" weight="bold">
           Screenshots
         </Text>
+        <Notification type="inform" icon="Info">
+          Screenshots must be in the JPG or PNG format, and in the RGB color space. You can add up to 8 screenshots for
+          each supported device type. To learn more,{' '}
+          <a href="https://support.google.com/googleplay/android-developer/answer/1078870?hl=en">click here</a>.
+        </Notification>
         <Icon name="Support" color="grape-3" paddingHorizontal="x1" size="2rem" />
       </Flex>
 
