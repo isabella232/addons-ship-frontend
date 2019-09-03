00013e1ced8eb0b1f9014bc4003c9e951e8aca6e
József Erős <jozsef.eros@bitrise.io>
2019-09-03 10:34:39 +0200

    Add notification about devices

--- pages/version/devices/view.tsx
+++ pages/version/devices/view.tsx
@@ -1,4 +1,14 @@
-import { Table, TableHeader, TableHeaderRow, TableHeaderCell, TableBody, TableRow, TableCell } from '@bitrise/bitkit';
+import {
+  Table,
+  TableHeader,
+  TableHeaderRow,
+  TableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Notification,
+  Base
+} from '@bitrise/bitkit';
 
 import { TestDevice } from '@/models/test-device';
 
@@ -8,25 +18,30 @@ type Props = {
 };
 
 export default ({ projectType, devices }: Props) => (
-  <Table type="card">
-    <TableHeader>
-      <TableHeaderRow>
-        <TableHeaderCell>User</TableHeaderCell>
-        <TableHeaderCell>Device</TableHeaderCell>
-        {projectType === 'ios' && <TableHeaderCell>UDID</TableHeaderCell>}
-        {projectType === 'android' && <TableHeaderCell>UUID</TableHeaderCell>}
-        {projectType !== 'ios' && projectType !== 'android' && <TableHeaderCell>Device ID</TableHeaderCell>}
-      </TableHeaderRow>
-    </TableHeader>
+  <Base paddingVertical="x6">
+    <Notification type="inform" icon="Lightbulb">
+      You can register your devices on your Account Settings page on Bitrise.
+    </Notification>
+    <Table type="card">
+      <TableHeader>
+        <TableHeaderRow>
+          <TableHeaderCell>User</TableHeaderCell>
+          <TableHeaderCell>Device</TableHeaderCell>
+          {projectType === 'ios' && <TableHeaderCell>UDID</TableHeaderCell>}
+          {projectType === 'android' && <TableHeaderCell>UUID</TableHeaderCell>}
+          {projectType !== 'ios' && projectType !== 'android' && <TableHeaderCell>Device ID</TableHeaderCell>}
+        </TableHeaderRow>
+      </TableHeader>
 
-    <TableBody>
-      {devices.map(({ deviceId, deviceType, owner }) => (
-        <TableRow key={deviceId}>
-          <TableCell>{owner}</TableCell>
-          <TableCell>{deviceType}</TableCell>
-          <TableCell>{deviceId}</TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
-  </Table>
+      <TableBody>
+        {devices.map(({ deviceId, deviceType, owner }) => (
+          <TableRow key={deviceId}>
+            <TableCell>{owner}</TableCell>
+            <TableCell>{deviceType}</TableCell>
+            <TableCell>{deviceId}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Base>
 );
