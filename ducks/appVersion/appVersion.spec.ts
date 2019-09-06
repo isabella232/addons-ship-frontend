a67fe432bc413423019eddd216d05a7fdd36d776
Simon Márton <simon.marton@bitrise.io>
2019-09-06 15:11:57 +0200

    Fetch featureGraphic for only android versions

--- ducks/appVersion/appVersion.spec.ts
+++ ducks/appVersion/appVersion.spec.ts
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk';
 import { TestScheduler } from 'rxjs/testing';
 import { getType } from 'deox';
 
-import { mockAppVersion, mockUploadedScreenshotResponse } from '@/mocks';
+import { mockAppVersion, mockUploadedScreenshotResponse, mockAndroidAppVersion } from '@/mocks';
 import { AppVersion, AppVersionEvent, AppVersionEventStatus } from '@/models';
 import api, { ShipAPIService } from '@/services/ship-api';
 import { uploadFileToS3 } from '@/utils/file';
@@ -101,6 +101,14 @@ describe('appVersion', () => {
       await store.dispatch(fetchAppVersion('app-slug', 'version-id') as any);
 
       expect(store.getActions()).toMatchSnapshot();
+    });
+
+    it('fetches feature graphic for an android app version', async () => {
+      (api.getAppVersion as jest.Mock).mockResolvedValueOnce(mockAndroidAppVersion);
+      (api.getScreenshots as jest.Mock).mockResolvedValueOnce([mockUploadedScreenshotResponse]);
+      await store.dispatch(fetchAppVersion('app-slug', 'version-id') as any);
+
+      expect(api.getFeatureGraphic).toHaveBeenCalledWith('app-slug', 'version-id');
     });
 
     it("can't fetch an app version", async () => {
