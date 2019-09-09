2e41c752cd389fdf85859575fb88d5fb9076fd5a
József Erős <jozsef.eros@bitrise.io>
2019-09-10 00:03:23 +0200

    Make devices responsive

--- pages/version/devices/view/index.tsx
+++ pages/version/devices/view/index.tsx
@@ -11,12 +11,14 @@ import {
   Skeleton,
   SkeletonBox,
   Divider,
-  Link
+  Link,
+  Text
 } from '@bitrise/bitkit';
 
 import { TestDevice } from '@/models/test-device';
 
 import { Fragment } from 'react';
+import { mediaQuery } from '@/utils/media';
 
 type Props = {
   projectType: string;
@@ -24,50 +26,79 @@ type Props = {
   isLoading: boolean;
 };
 
-export default ({ projectType, devices, isLoading }: Props) => (
-  <Base paddingVertical="x6">
-    <Notification type="inform" icon="Lightbulb">
-      You can register your devices on your{' '}
-      <Link href="https://app.bitrise.io/me/profile#/test_devices" target="_blank" underline>
-        Account Settings
-      </Link>{' '}
-      page on Bitrise.
-    </Notification>
-    {isLoading ? (
-      <Base margin="x8">
-        <Skeleton active>
-          {[...Array(5)].map((_, idx) => (
-            <Fragment key={idx}>
-              <Base paddingHorizontal="x4" paddingVertical="x3">
-                <SkeletonBox height="1.5rem" margin="x2" />
-              </Base>
-              <Divider width="1px" color="gray-2" />
-            </Fragment>
-          ))}
-        </Skeleton>
-      </Base>
-    ) : (
-      <Table type="flat" paddingVertical="x6">
-        <TableHeader>
-          <TableHeaderRow>
-            <TableHeaderCell>User</TableHeaderCell>
-            <TableHeaderCell>Device</TableHeaderCell>
-            {projectType === 'ios' && <TableHeaderCell>UDID</TableHeaderCell>}
-            {projectType === 'android' && <TableHeaderCell>UUID</TableHeaderCell>}
-            {projectType !== 'ios' && projectType !== 'android' && <TableHeaderCell>Device ID</TableHeaderCell>}
-          </TableHeaderRow>
-        </TableHeader>
+export default ({ projectType, devices, isLoading }: Props) => {
+  const [isTablet] = mediaQuery('30rem');
 
-        <TableBody>
-          {devices.map(({ deviceId, deviceType, owner }) => (
-            <TableRow key={`${owner}-${deviceId}`}>
-              <TableCell>{owner}</TableCell>
-              <TableCell>{deviceType}</TableCell>
-              <TableCell>{deviceId}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    )}
-  </Base>
-);
+  const listItem = (label: string, value: string, isLast = false) => {
+    return isTablet ? (
+      <TableCell>{value}</TableCell>
+    ) : (
+      <TableCell style={{ display: 'block', borderBottom: isLast ? undefined : 'none', paddingTop: '0.25rem', paddingBottom: '0.25rem' }}>
+        <Text weight="bold" color="grape-5" inline>
+          {label}:
+        </Text>{' '}
+        <Text breakOn="all" inline>{value}</Text>
+      </TableCell>
+    );
+  };
+
+  const userLabel = 'User';
+  const deviceLabel = 'Device';
+  let deviceIDLabel = 'Device ID';
+  switch (projectType) {
+    case 'ios':
+      deviceIDLabel = 'UDID';
+      break;
+    case 'android':
+      deviceIDLabel = 'UUID';
+      break;
+  }
+
+  return (
+    <Base paddingVertical="x6">
+      <Notification type="inform" icon="Lightbulb">
+        You can register your devices on your{' '}
+        <Link href="https://app.bitrise.io/me/profile#/test_devices" target="_blank" underline>
+          Account Settings
+        </Link>{' '}
+        page on Bitrise.
+      </Notification>
+      {isLoading ? (
+        <Base margin="x8">
+          <Skeleton active>
+            {[...Array(5)].map((_, idx) => (
+              <Fragment key={idx}>
+                <Base paddingHorizontal="x4" paddingVertical="x3">
+                  <SkeletonBox height="1.5rem" margin="x2" />
+                </Base>
+                <Divider width="1px" color="gray-2" />
+              </Fragment>
+            ))}
+          </Skeleton>
+        </Base>
+      ) : (
+        <Table type="flat" paddingVertical="x6">
+          {isTablet && (
+            <TableHeader>
+              <TableHeaderRow>
+                <TableHeaderCell>{userLabel}</TableHeaderCell>
+                <TableHeaderCell>{deviceLabel}</TableHeaderCell>
+                <TableHeaderCell>{deviceIDLabel}</TableHeaderCell>
+              </TableHeaderRow>
+            </TableHeader>
+          )}
+
+          <TableBody>
+            {devices.map(({ deviceId, deviceType, owner }) => (
+              <TableRow key={`${owner}-${deviceId}`}>
+                {listItem(userLabel, owner)}
+                {listItem(deviceLabel, deviceType)}
+                {listItem(deviceIDLabel, deviceId, true)}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </Base>
+  );
+};
