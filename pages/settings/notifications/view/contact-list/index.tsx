221455bd58cf932db07dcceb1a322cbda1391e4c
József Erős <jozsef.eros@bitrise.io>
2019-09-10 09:01:25 +0200

    Make notification settings responsive

--- pages/settings/notifications/view/contact-list/index.tsx
+++ pages/settings/notifications/view/contact-list/index.tsx
@@ -8,11 +8,15 @@ import {
   TableRow,
   Toggle,
   Base,
-  Icon
+  Icon,
+  Text,
+  Flex
 } from '@bitrise/bitkit';
 
 import css from './style.scss';
 import { AppContact } from '@/models/settings';
+import { mediaQuery } from '@/utils/media';
+import { Fragment } from 'react';
 
 export type Props = {
   appContacts: AppContact[];
@@ -20,71 +24,109 @@ export type Props = {
   onDeleteContact: (email: string) => void;
 };
 
-export default ({ appContacts, onNotificationPreferenceChanged, onDeleteContact }: Props) => (
-  <Table type="flat">
-    <TableHeader>
-      <TableHeaderRow>
-        <TableHeaderCell>Email</TableHeaderCell>
-        <TableHeaderCell />
-        <TableHeaderCell align="middle">
-          New
-          <br />
-          Version
-        </TableHeaderCell>
-        <TableHeaderCell align="middle">
-          Successful
-          <br />
-          Publish
-        </TableHeaderCell>
-        <TableHeaderCell align="middle">
-          Failed
-          <br />
-          Publish
-        </TableHeaderCell>
-        <TableHeaderCell />
-      </TableHeaderRow>
-    </TableHeader>
-    <TableBody>
-      {appContacts.map(
-        ({
-          email,
-          isConfirmed,
-          isMarkedForDelete,
-          notificationPreferences: { newVersion, successfulPublish, failedPublish }
-        }) =>
-          !isMarkedForDelete && (
-            <TableRow key={email}>
-              <TableCell shrink>{email}</TableCell>
-              <TableCell align="middle">{isConfirmed ? 'Confirmed' : 'Pending'}</TableCell>
-              <TableCell shrink>
-                <Toggle
-                  checked={newVersion}
-                  onChange={() => onNotificationPreferenceChanged(email, 'newVersion', !newVersion)}
-                  alignChildren="middle"
-                />
-              </TableCell>
-              <TableCell shrink>
-                <Toggle
-                  checked={successfulPublish}
-                  onChange={() => onNotificationPreferenceChanged(email, 'successfulPublish', !successfulPublish)}
-                  alignChildren="middle"
-                />
-              </TableCell>
-              <TableCell shrink>
-                <Toggle
-                  checked={failedPublish}
-                  onChange={() => onNotificationPreferenceChanged(email, 'failedPublish', !failedPublish)}
-                  alignChildren="middle"
-                />
-              </TableCell>
-              <TableCell shrink>
-                <Base padding="x1" className={css.delete} borderRadius="x1" onClick={() => onDeleteContact(email)}>
-                  <Icon name="Trash" size="1.5rem" color="red-3" />
-                </Base>
-              </TableCell>
-            </TableRow>
-          )
+export default ({ appContacts, onNotificationPreferenceChanged, onDeleteContact }: Props) => {
+  const [isDesktop] = mediaQuery('60rem');
+
+  const toggle = (email: string, preferenceID: string, preferenceValue: boolean) => (
+    <Toggle
+      checked={preferenceValue}
+      onChange={() => onNotificationPreferenceChanged(email, preferenceID, !preferenceValue)}
+      alignChildren="middle"
+    />
+  );
+
+  return (
+    <Table type="flat">
+      {isDesktop && (
+        <TableHeader>
+          <TableHeaderRow>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell />
+            <TableHeaderCell align="middle">
+              New
+              <br />
+              Version
+            </TableHeaderCell>
+            <TableHeaderCell align="middle">
+              Successful
+              <br />
+              Publish
+            </TableHeaderCell>
+            <TableHeaderCell align="middle">
+              Failed
+              <br />
+              Publish
+            </TableHeaderCell>
+            <TableHeaderCell />
+          </TableHeaderRow>
+        </TableHeader>
       )}
-    </TableBody>
-  </Table>
-);
+      <TableBody>
+        {appContacts.map(
+          ({
+            email,
+            isConfirmed,
+            isMarkedForDelete,
+            notificationPreferences: { newVersion, successfulPublish, failedPublish }
+          }) =>
+            !isMarkedForDelete && (
+              <TableRow key={email}>
+                {isDesktop ? (
+                  <Fragment>
+                    <TableCell shrink>{email}</TableCell>
+                    <TableCell align="middle">{isConfirmed ? 'Confirmed' : 'Pending'}</TableCell>
+                    <TableCell shrink>{toggle(email, 'newVersion', newVersion)}</TableCell>
+                    <TableCell shrink>{toggle(email, 'successfulPublish', successfulPublish)}</TableCell>
+                    <TableCell shrink>{toggle(email, 'failedPublish', failedPublish)}</TableCell>
+                    <TableCell shrink>
+                      <Base
+                        padding="x1"
+                        className={css.delete}
+                        borderRadius="x1"
+                        onClick={() => onDeleteContact(email)}
+                      >
+                        <Icon name="Trash" size="1.5rem" color="red-3" />
+                      </Base>
+                    </TableCell>
+                  </Fragment>
+                ) : (
+                  <TableCell>
+                    <Base paddingVertical="x1">
+                      <Text weight="bold" color="grape-5" inline>
+                        Email:
+                      </Text>{' '}
+                      {email}
+                    </Base>
+                    <Base paddingVertical="x1">
+                      <Text weight="bold" color="grape-5" inline>
+                        Status:
+                      </Text>{' '}
+                      {isConfirmed ? 'Confirmed' : 'Pending'}
+                    </Base>
+                    <Flex direction="horizontal" gap="x2" paddingVertical="x1">
+                      <Text weight="bold" color="grape-5" width="9rem">
+                        New Version:
+                      </Text>{' '}
+                      {toggle(email, 'newVersion', newVersion)}
+                    </Flex>
+                    <Flex direction="horizontal" gap="x2" paddingVertical="x1">
+                      <Text weight="bold" color="grape-5" width="9rem">
+                        Successful Publish:
+                      </Text>{' '}
+                      {toggle(email, 'successfulPublish', successfulPublish)}
+                    </Flex>
+                    <Flex direction="horizontal" gap="x2" paddingVertical="x1">
+                      <Text weight="bold" color="grape-5" width="9rem">
+                        Failed Publish:
+                      </Text>{' '}
+                      {toggle(email, 'failedPublish', failedPublish)}
+                    </Flex>
+                  </TableCell>
+                )}
+              </TableRow>
+            )
+        )}
+      </TableBody>
+    </Table>
+  );
+};
--- pages/settings/notifications/view/index.tsx
+++ pages/settings/notifications/view/index.tsx
@@ -10,6 +10,7 @@ import {
   Skeleton,
   ProgressSpinner
 } from '@bitrise/bitkit';
+import { mediaQuery } from '@/utils/media';
 
 import AddNew, { Props as AddNewProps } from './add-new';
 import List, { Props as ListProps } from './contact-list';
@@ -35,58 +36,62 @@ export default ({
   onSave,
   onCancel,
   error
-}: Props) => (
-  <Base paddingVertical="x8">
-    <Text size="x5" weight="bold">
-      Email Notifications
-    </Text>
-    <Divider color="gray-2" margin="x4" />
-    {error && (
-      <Notification margin="x4" type="alert">
-        {error}
-      </Notification>
-    )}
-    <Base margin="x6">
-      <AddNew email={email} onEmailChange={onEmailChange} onAddEmail={onAddEmail} isAddingEmail={isAddingEmail} />
-    </Base>
+}: Props) => {
+  const [isTablet] = mediaQuery('30rem');
 
-    {hasLoaded ? (
-      <Fragment>
-        <Base margin="x4">
-          <List
-            appContacts={appContacts}
-            onNotificationPreferenceChanged={onNotificationPreferenceChanged}
-            onDeleteContact={onDeleteContact}
-          />
-        </Base>
-        <Flex margin="x12" direction="horizontal" alignChildrenHorizontal="end" gap="x4">
-          <Button disabled={!onCancel || isSaving} level="secondary" width="8rem" onClick={onCancel}>
-            Cancel
-          </Button>
-          <Button disabled={!onSave || isSaving} level="primary" width="8rem" onClick={onSave}>
-            {isSaving ? (
-              <Fragment>
-                <ProgressSpinner /> &nbsp; Saving...
-              </Fragment>
-            ) : (
-              'Save'
-            )}
-          </Button>
-        </Flex>
-      </Fragment>
-    ) : (
-      <Base margin="x16">
-        <Skeleton active maxWidth="60rem">
-          {[...Array(5)].map((_, idx) => (
-            <Fragment key={idx}>
-              <Base paddingHorizontal="x4" paddingVertical="x3">
-                <SkeletonBox height="1.5rem" margin="x2" />
-              </Base>
-              <Divider width="1px" color="gray-2" />
-            </Fragment>
-          ))}
-        </Skeleton>
+  return (
+    <Base paddingVertical="x8">
+      <Text size="x5" weight="bold">
+        Email Notifications
+      </Text>
+      <Divider color="gray-2" margin="x4" />
+      {error && (
+        <Notification margin="x4" type="alert">
+          {error}
+        </Notification>
+      )}
+      <Base margin="x6">
+        <AddNew email={email} onEmailChange={onEmailChange} onAddEmail={onAddEmail} isAddingEmail={isAddingEmail} />
       </Base>
-    )}
-  </Base>
-);
+
+      {hasLoaded ? (
+        <Fragment>
+          <Base margin="x4">
+            <List
+              appContacts={appContacts}
+              onNotificationPreferenceChanged={onNotificationPreferenceChanged}
+              onDeleteContact={onDeleteContact}
+            />
+          </Base>
+          <Flex margin="x12" direction="horizontal" alignChildrenHorizontal="end" gap="x4">
+            <Button disabled={!onCancel || isSaving} level="secondary" width="8rem" grow={!isTablet} onClick={onCancel}>
+              Cancel
+            </Button>
+            <Button disabled={!onSave || isSaving} level="primary" width="8rem" grow={!isTablet} onClick={onSave}>
+              {isSaving ? (
+                <Fragment>
+                  <ProgressSpinner /> &nbsp; Saving...
+                </Fragment>
+              ) : (
+                'Save'
+              )}
+            </Button>
+          </Flex>
+        </Fragment>
+      ) : (
+        <Base margin="x16">
+          <Skeleton active maxWidth="60rem">
+            {[...Array(5)].map((_, idx) => (
+              <Fragment key={idx}>
+                <Base paddingHorizontal="x4" paddingVertical="x3">
+                  <SkeletonBox height="1.5rem" margin="x2" />
+                </Base>
+                <Divider width="1px" color="gray-2" />
+              </Fragment>
+            ))}
+          </Skeleton>
+        </Base>
+      )}
+    </Base>
+  );
+};
