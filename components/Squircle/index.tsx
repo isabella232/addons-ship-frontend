89c41e116a594691a47f07555f86179247b985be
Simon Márton <simon.marton@bitrise.io>
2019-10-02 10:27:26 +0200

    Fix Squircle for iOS Safari

--- components/Squircle/index.tsx
+++ components/Squircle/index.tsx
@@ -6,7 +6,7 @@ interface Props extends BaseProps {
   objectFit?: string;
 }
 
-export default ({ objectFit = 'covver', fatness = 0.02, ...props }: Props) => {
+export default ({ objectFit = 'cover', fatness = 0.02, ...props }: Props) => {
   const s = Math.min(Math.max(fatness, 0), 0.5);
 
   const path = `M .5,0
@@ -16,6 +16,17 @@ export default ({ objectFit = 'covver', fatness = 0.02, ...props }: Props) => {
     1,${s} ${1 - s},0 .5,0
     Z`;
   const id = `squircle-${fatness}`;
+
+  // This is a hacky fix to include -webkit-clip-path for iOS devices
+  const styleRef = (node: HTMLImageElement | null) => {
+    if (node !== null) {
+      const style = node.getAttribute('style');
+
+      if (!style!.includes('-webkit-clip-path')) {
+        node.setAttribute('style', `${style} -webkit-clip-path: url("#${id}");`);
+      }
+    }
+  };
 
   return (
     <div>
@@ -27,7 +38,7 @@ export default ({ objectFit = 'covver', fatness = 0.02, ...props }: Props) => {
         </defs>
       </svg>
 
-      <Image {...props} style={{ clipPath: `url(#${id})`, objectFit }} maxWidth="none" />
+      <Image {...props} style={{ clipPath: `url(#${id})`, objectFit }} innerRef={styleRef} maxWidth="none" />
     </div>
   );
 };
