0a47a32c92e855d5ba99ce3634d141549bdd7c84
Simon Márton <simon.marton@bitrise.io>
2019-10-03 15:58:11 +0200

    Added Hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript, NextDocumentContext } from 'next/document';
 
+const isProdMode = process.env.NODE_ENV === 'production';
+
 export default class ShipDocument extends Document {
   static async getInitialProps(ctx: NextDocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -23,6 +25,22 @@ export default class ShipDocument extends Document {
 
           {/* iOS */}
           <link rel="apple-touch-icon" href="path/to/favicon-180.png" sizes="180x180" />
+
+          {/* Hotjar */}
+          {isProdMode && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:236822,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+              }}
+            ></script>
+          )}
         </Head>
         <body>
           <Main />
